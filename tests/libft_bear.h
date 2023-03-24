edb19ca6259654f29faf06a9ab8d9031044c58a7
Markus Kurz <makurz@3-B-9.42heilbronn.de>
2023-03-24 11:25:22 +0100

    updated libft_bear and .gitignore file

--- tests/libft_bear.h
+++ tests/libft_bear.h
@@ -6,14 +6,20 @@
 /*   By: makurz <makurz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 14:27:36 by makurz            #+#    #+#             */
-/*   Updated: 2023/03/24 10:57:47 by makurz           ###   ########.fr       */
+/*   Updated: 2023/03/24 11:22:42 by makurz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_TEST
 # define LIBFT_TEST
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <limits.h>
+
 // do {} while (0) to group multiple statements into one block!
-# define ASSERT_EQUAL(expected, actual) \
+# define ASSERT_EQUAL_int(expected, actual) \
 	do \
 	{ \
 		if ((expected) != (actual)) \
@@ -58,33 +64,7 @@
 		} \
 	} while (0)
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <limits.h>
-
-// void	ft_isalpha_test(void);
-// void	ft_isdigit_test(void);
-// void	ft_isascii_test(void);
-// void	ft_isprint_test(void);
-// void	ft_isalnum_test(void);
-// void	ft_tolower_test(void);
-// void	ft_toupper_test(void);
-// void	ft_atoi_test(void);
-// void	ft_putchar_fd_test(char c, int fd);
-// void	ft_putstr_fd_test(char *s, int fd);
-// void	ft_putendl_fd_test(char *s, int fd);
-// void	ft_strlen_test(void);
-// void	*ft_memset_test(void *b, int c, size_t len);
-// void	ft_bzero_test(void *s, size_t n);
-// void	ft_memcmp_test(void);
-// void	*ft_memcpy_test(void *dst, const void *src, size_t n);
-// void	*ft_memchr_test(const void *s, int c, size_t n);
-// void	ft_strncmp_test(void);
-// char	*ft_strdup_test(const char *s1);
-// void	*ft_calloc_test(size_t count, size_t size);
-// size_t	ft_strlcat_test(char *dst, const char *src, size_t dstsize);
-// size_t	ft_strlcpy_test(char *dst, const char *src, size_t dstsize);
-// char	*ft_strjoin_test(char const *s1, char const *s2);
+// function to limit the allocatable memory amount of malloc()
+void	*limited_malloc(size_t size);
 
 #endif
