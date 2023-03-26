bf4638b18703f31442febf523e8998354e6b529f
kurz-m <m.kurz92@gmail.com>
2023-03-26 13:16:21 +0200

    added test for itoa and updated libft_bear for a new test

--- tests/libft_bear.h
+++ tests/libft_bear.h
@@ -6,7 +6,7 @@
 /*   By: makurz <makurz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 14:27:36 by makurz            #+#    #+#             */
-/*   Updated: 2023/03/24 11:22:42 by makurz           ###   ########.fr       */
+/*   Updated: 2023/03/26 13:16:13 by makurz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,12 @@
 	{ \
 		if ((expected) != (actual)) \
 		{ \
-			printf("\033[31mx\033[0m Test failed in %s at line %d: expected %d but got %d\n", \
+			printf("\033[31mx\033[0m Test failed in %s at line %d: expected %d but got %d\n.", \
 			__FILE__, __LINE__, expected, actual); \
 		} \
 		if ((expected) == (actual)) \
 		{ \
-			printf("\033[32m\xE2\x9C\x93\033[0m Test passed in %s at line %d: expected %d actual %d\n", \
+			printf("\033[32m\xE2\x9C\x93\033[0m Test passed in %s at line %d: expected %d actual %d\n.", \
 			__FILE__, __LINE__, expected, actual); \
 		} \
 	} while (0)
@@ -39,12 +39,12 @@
 	{ \
 		if ((expected) != (actual)) \
 		{ \
-			printf("\033[31mx\033[0m Test failed in %s at line %d: expected %zu but got %zu\n", \
+			printf("\033[31mx\033[0m Test failed in %s at line %d: expected %zu but got %zu\n.", \
 			__FILE__, __LINE__, expected, actual); \
 		} \
 		if ((expected) == (actual)) \
 		{ \
-			printf("\033[32m\xE2\x9C\x93\033[0m Test passed in %s at line %d: expected %zu actual %zu\n", \
+			printf("\033[32m\xE2\x9C\x93\033[0m Test passed in %s at line %d: expected %zu actual %zu\n.", \
 			__FILE__, __LINE__, expected, actual); \
 		} \
 	} while (0)
@@ -54,12 +54,27 @@
 	{ \
 		if (((expected > 0) && (actual < 0)) || ((expected < 0) && (actual > 0)) || ((expected == 0) && (actual != 0))) \
 		{ \
-			printf("\033[31mx\033[0m Test failed in %s at line %d: expected %d but got %d\n", \
+			printf("\033[31mx\033[0m Test failed in %s at line %d: expected %d but got %d\n.", \
 			__FILE__, __LINE__, expected, actual); \
 		} \
 		else if (((expected > 0) && (actual > 0)) || ((expected < 0) && (actual < 0)) || (expected == actual)) \
 		{ \
-			printf("\033[32m\xE2\x9C\x93\033[0m Test passed in %s at line %d: expected %d actual %d\n", \
+			printf("\033[32m\xE2\x9C\x93\033[0m Test passed in %s at line %d: expected %d actual %d\n.", \
+			__FILE__, __LINE__, expected, actual); \
+		} \
+	} while (0)
+
+# define ASSERT_EQUAL_str(expected, actual) \
+	do \
+	{ \
+		if (strcmp(expected, actual)) \
+		{ \
+			printf("\033[31mx\033[0m Test failed in %s at line %d: expected %s but got %s\n.", \
+			__FILE__, __LINE__, expected, actual); \
+		} \
+		else if (!strcmp(expected, actual)) \
+		{ \
+			printf("\033[32m\xE2\x9C\x93\033[0m Test passed in %s at line %d: expected %s actual %s\n.", \
 			__FILE__, __LINE__, expected, actual); \
 		} \
 	} while (0)
