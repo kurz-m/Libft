ca8dd87b3237bfec4056bbe0bd550d11cc5fbae3
Markus Kurz <makurz@3-B-11.42heilbronn.de>
2023-04-17 12:16:13 +0200

    small fix in strdup

--- src/string/ft_strdup.c
+++ src/string/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: makurz <makurz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 16:25:05 by makurz            #+#    #+#             */
-/*   Updated: 2023/03/27 21:20:48 by makurz           ###   ########.fr       */
+/*   Updated: 2023/04/17 12:15:20 by makurz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_strdup(const char *s1)
 
 	len = ft_strlen(s1) + 1;
 	dst = (char *) malloc(sizeof(char) * len);
-	if (!dst)
+	if (dst == NULL)
 		return (NULL);
 	ft_strlcpy(dst, s1, len);
 	return (dst);
