96f4b3a04f93c0d5ae15c4b7d1d4d8f0a249f91e
kurz-m <m.kurz92@gmail.com>
2023-04-28 07:24:17 +0200

    finished new split

--- src/string/string_update.c
+++ src/string/string_update.c
@@ -6,98 +6,54 @@
 /*   By: makurz <dumba@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 06:55:14 by makurz            #+#    #+#             */
-/*   Updated: 2023/04/28 06:57:55 by makurz           ###   ########.fr       */
+/*   Updated: 2023/04/28 07:22:50 by makurz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../header/libft.h"
 
-size_t	ft_strlen_c(const char *str)
-{
-	size_t	i;
-
-	i = 0;
-	while (*str && !ft_isspace(*str++))
-		++i;
-	return (i);
-}
-
-int	ft_count_words(const char *str)
+static int	ft_count_words(const char *str, char c)
 {
 	int		count;
 
 	count = 0;
 	while (*str)
 	{
-		if (ft_isspace(*str))
+		if (*str == c)
 			str++;
 		else
 		{
 			count++;
-			str += ft_strlen_c(str);
+			str += ft_strlen_c(str, 32);
 		}
 	}
 	return (count);
 }
 
-void	ft_arrfree(char **arr, size_t len)
+char	**maintmp(char const *str, char c)
 {
-	size_t	i;
-
-	i = 0;
-	while (i < len)
-	{
-		free(arr[i++]);
-		arr[i] = NULL;
-	}
-	free(arr);
-	arr = NULL;
-}
-
-void	ft_putstr(char *str)
-{
-	write(1, str, ft_strlen(str));
-}
-
-int	main(int argc, char *argv[])
-{
-	char	*str;
 	int		count;
 	char	**arr;
 	int		i;
-	int		clean;
 
-	if (argc != 2)
-		return (write(1, "\n", 1), 1);
-	str = argv[1];
 	i = 0;
-	count = ft_count_words(str);
-	arr = malloc(sizeof(char*) * (count + 1));
+	count = ft_count_words(str, c);
+	arr = ft_calloc(sizeof(char*), (count + 1));
 	if (arr == NULL)
-		return (1);
+		return (NULL);
 	arr[count] = NULL;
 	while (i < count)
 	{
-		if (ft_isspace(*str))
+		if (*str == c)
 			str++;
 		else
 		{
-			arr[i] = ft_strdup(str, ft_strlen_c(str));
+			arr[i] = ft_substr(str, 0, ft_strlen_c(str, 32));
 			if (arr[i] == NULL)
-				return (ft_arrfree(arr, (count - i)), 1);
-			str += ft_strlen_c(str);
+				return (ft_arrfree(arr), NULL);
+			str += ft_strlen_c(str, 32);
 			++i;
 		}
 	}
-	clean = count;
-	--count;
-	while (count >= 0)
-	{
-		ft_putstr(arr[count]);
-		if (count != 0)
-			write(1, " ", 1);
-		--count;
-	}
-	ft_arrfree(arr, clean);
-	return (write(1, "\n", 1), 0);
+	return (arr);
 }
