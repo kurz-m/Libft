c313a2d35aa47fa2680ff93b71b28beab6dfe0f5
kurz-m <m.kurz92@gmail.com>
2023-07-03 12:57:46 +0200

    update memcpy

--- src/memory/ft_memcpy.c
+++ src/memory/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: makurz <makurz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 14:02:58 by makurz            #+#    #+#             */
-/*   Updated: 2023/04/18 22:16:35 by makurz           ###   ########.fr       */
+/*   Updated: 2023/07/03 12:57:22 by makurz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,13 @@
 // Copies 'n' bytes from memory space 'src' to memory space 'dst'.
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t				i;
-	const unsigned char	*tmp_src;
-	unsigned char		*tmp_dst;
+	const char	*s = src;
+	char		*d;
 
-	i = 0;
 	if (dst == NULL && src == NULL)
 		return (NULL);
-	tmp_src = (const unsigned char *) src;
-	tmp_dst = (unsigned char *) dst;
-	while (i++ < n)
-		*tmp_dst++ = *tmp_src++;
+	d = (char *) dst;
+	while (n--)
+		*d++ = *s++;
 	return (dst);
 }
