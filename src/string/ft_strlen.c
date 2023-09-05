26777c9d1f6a92a475fbfa6315f17921f2a4226b
kurz-m <m.kurz92@gmail.com>
2023-09-05 22:39:15 +0200

    update strlen for char * pointing to NULL

--- src/string/ft_strlen.c
+++ src/string/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: makurz <makurz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 14:38:56 by makurz            #+#    #+#             */
-/*   Updated: 2023/03/27 14:27:02 by makurz           ###   ########.fr       */
+/*   Updated: 2023/09/05 19:41:22 by makurz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ size_t	ft_strlen(const char *s)
 {
 	size_t	i;
 
+	if (NULL == s)
+		return (0);
 	i = 0;
 	while (s[i])
 		i++;
