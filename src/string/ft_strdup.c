8d74f6f9bfe52239695838a52c5697a9b6139087
kurz-m <m.kurz92@gmail.com>
2023-09-21 22:20:05 +0200

    modified strndup

--- src/string/ft_strdup.c
+++ src/string/ft_strdup.c
@@ -6,31 +6,22 @@
 /*   By: makurz <makurz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 16:25:05 by makurz            #+#    #+#             */
-/*   Updated: 2023/09/21 11:15:15 by makurz           ###   ########.fr       */
+/*   Updated: 2023/09/21 22:19:55 by makurz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 // Returns a freeable copy of the string 's1'.
-// char	*ft_strdup(const char *s1)
-// {
-// 	size_t	len;
-// 	char	*dst;
-// 
-// 	len = ft_strlen(s1) + 1;
-// 	dst = (char *) malloc(sizeof(char) * len);
-// 	if (dst == NULL)
-// 		return (NULL);
-// 	ft_memcpy(dst, s1, len);
-// 	ft_strlcpy(dst, s1, len);
-// 	return (dst);
-// }
-
 char	*ft_strdup(const char *s1)
 {
 	size_t	len;
+	char	*dst;
 
 	len = ft_strlen(s1) + 1;
-	return (ft_strndup(s1, len));
+	dst = malloc(sizeof(char) * len);
+	if (dst == NULL)
+		return (NULL);
+	ft_memcpy(dst, s1, len);
+	return (dst);
 }
