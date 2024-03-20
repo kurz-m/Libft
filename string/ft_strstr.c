8e84239304e47dcf30504d393fcfe6e7f4d173ca
Markus <markus.kurz@r-stahl.com>
2024-03-20 19:13:38 +0100

    start strstr

--- string/ft_strstr.c
+++ string/ft_strstr.c
@@ -13,25 +13,30 @@
 
    The MIT Licence will be situated within the root directory. */
 
-#include "libft.h"
+#include "string.h"
 
 // Returns the first occurrence of 'needle' in the string 'haystack'.
-char	*ft_strstr(const char *haystack, const char *needle)
-{
-	char	*ptr;
-	size_t	n_len;
-	size_t	h_len;
+char *ft_strstr(const char *haystack, const char *needle) {
+  const unsigned char *hs = (const unsigned char *)haystack;
+  const unsigned char *ne = (const unsigned char *)needle;
 
-	n_len = ft_strlen(needle);
-	if (n_len == 0)
-		return ((char *) haystack);
-	h_len = ft_strlen(haystack);
-	ptr = (char *) haystack;
-	while (*ptr && h_len-- >= n_len)
-	{
-		if (*ptr == *needle && !ft_strncmp(ptr, needle, n_len))
-			return (ptr);
-		ptr++;
-	}
-	return (NULL);
+  if (ne[0] == '\0')
+    return (char *)hs;
+  hs = (const unsigned char *)ft_strchr((const char *)hs, ne[0]);
+  if (hs == NULL || ne[1] == '\0')
+    return (char *)hs;
+  char *ptr;
+  size_t n_len = ft_strlen(needle);
+  size_t src_len;
+
+  if (n_len == 0)
+    return ((char *)haystack);
+  size_t src_len = ft_strlen(haystack);
+  ptr = (char *)haystack;
+  while (*ptr && src_len-- >= n_len) {
+    if (*ptr == *needle && !ft_strncmp(ptr, needle, n_len))
+      return (ptr);
+    ptr++;
+  }
+  return (NULL);
 }
