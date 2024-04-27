40640fc4abbbdbe002439205b301dde6d26668fa
makurz <makurz@student.42heilbronn.de>
2024-04-28 00:39:57 +0200

    make memcpy working

--- string/ft_memcpy.h
+++ string/ft_memcpy.h
@@ -22,16 +22,29 @@ typedef unsigned char byte;
 
 /* Copy exactly nbytes by copying each single byte from src_bp to dst_bp.
    This function is mostly used for aligning the memory addresses. */
-static inline void *byte_copy_fwd(void *dst, const void *src, size_t n)
+static inline void byte_copy_fwd(void **dst, const void **src, size_t n)
 {
-  const byte *s = (const byte *)src;
-  byte *d = (byte *)dst;
+  const byte *s = (const byte *)*src;
+  byte *d = (byte *)*dst;
 
   while (n-- != 0)
   {
     *d++ = *s++;
   }
-  return dst;
+  *dst = d;
+  *src = s;
 }
+//
+// #define BYTE_COPY_FWD(dst_bp, src_bp, nbytes)                                  \
+//   do {                                                                         \
+//     size_t __nbytes = (nbytes);                                                \
+//     while (__nbytes > 0) {                                                     \
+//       byte __x = ((byte *)src_bp)[0];                                          \
+//       ++src_bp;                                                                \
+//       --__nbytes;                                                              \
+//       ((byte *)dst_bp)[0] = __x;                                               \
+//       ++dst_bp;                                                                \
+//     }                                                                          \
+//   } while (0)
 
 #endif /* ft_memcpy.h */
